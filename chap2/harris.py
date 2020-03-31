4c5b5572d01e13a42eefb2423e66e34675c305cb
松岡 良春 <y-matsuoka@SISCO.local>
2020-03-31 09:13:02 +0900

    chap2.1終了

--- chap2/harris.py
+++ chap2/harris.py
@@ -52,14 +52,98 @@ def get_harris_points(harrisim, min_dist=10, threshold=0.1):
             filtered_coords.append(coords[i])
             allowed_locations[(coords[i, 0]-min_dist):(coords[i, 0]+min_dist),
                               (coords[i, 1]-min_dist):(coords[i, 1]+min_dist)] = 0
-    
+
     return filtered_coords
+
 
 def plot_harris_points(image, filtered_coords):
     """画像中に見つかったコーナーを描画する"""
     figure()
     gray()
     imshow(image)
-    plot([p[1] for p in filtered_coords],[p[0] for p in filtered_coords],"*")
+    plot([p[1] for p in filtered_coords], [p[0] for p in filtered_coords], "*")
     axis("off")
     show()
+
+
+def get_descriptors(image, filtered_coords, wid=5):
+    """各点について、点の周辺で幅　2*wid+1　の近傍ピクセル値を返す
+    (点の最小距離　min_distance > wid　を仮定する)"""
+    desc = []
+    for coords in filtered_coords:
+        patch = image[coords[0]-wid:coords[0]+wid+1,
+                      coords[1]-wid:coords[1]+wid+1].flatten()
+        desc.append(patch)
+
+    return desc
+
+
+def match(desc1, desc2, threshold=0.5):
+    """正規化相互相関を用いて、第1の画像の各コーナー記述子について、
+    第2の画像の対点を選択する"""
+    n = len(desc1[0])
+
+    # 対応点ごとの距離
+    d = -ones((len(desc1), len(desc2)))
+    for i in range(len(desc1)):
+        for j in range(len(desc2)):
+            d1 = (desc1[i] - mean(desc1[i])) / std(desc1[i])
+            d2 = (desc2[j] - mean(desc2[j])) / std(desc2[j])
+            ncc_value = sum(d1 * d2) / (n - 1)
+            if ncc_value > threshold:
+                d[i, j] = ncc_value
+
+    ndx = argsort(-d)
+    matchscores = ndx[:, 0]
+
+    return matchscores
+
+
+def match_twosided(desc1, desc2, threshold=0.5):
+    """match()の双方向で一致を調べるバージョン"""
+    matches_12 = match(desc1, desc2, threshold)
+    matches_21 = match(desc2, desc1, threshold)
+
+    ndx_12 = where(matches_12 >= 0)[0]
+
+    # 非対称の場合を除去する
+    for n in ndx_12:
+        if matches_21[matches_12[n]] != n:
+            matches_12[n] = -1
+
+    return matches_12
+
+
+def appendimages(im1, im2):
+    """2つの画像を左右に並べた画像を返す"""
+
+    # 行の少ない法を選び、空行を0で埋める
+    rows1 = im1.shape[0]
+    rows2 = im2.shape[0]
+
+    if rows1 < rows2:
+        im1 = concatenate((im1, zeros((rows2-rows1, im1.shape[1]))), axis=0)
+    elif rows1 > rows2:
+        im2 = concatenate((im2, zeros((rows1-rows2, im2.shape[1]))), axis=0)
+    # 行が同じなら、0で埋める必要はない
+
+    return concatenate((im1, im2), axis=1)
+
+
+def plot_matches(im1, im2, locs1, locs2, matchscores, show_below=True):
+    """対応点を線で結んで画像を表示する
+    入力：im1、im2（配列形式の画像）、locs1、locs2（特徴点座標）、
+    matchscores（match()の出力）、
+    show_below（対応の下に画像を表示するならTrue）"""
+
+    im3 = appendimages(im1,im2)
+    if show_below:
+        im3 = vstack((im3,im3))
+    
+    imshow(im3)
+
+    cols1 = im1.shape[1]
+    for i,m in enumerate(matchscores):
+        if m > 0:
+            plot([locs1[i][1],locs2[m][1]+cols1],[locs1[i][0],locs2[m][0]],"c")
+    axis("off")
--- chap2/main.py
+++ chap2/main.py
@@ -2,8 +2,22 @@ from PIL import Image
 from numpy import *
 import harris
 
-im = array(Image.open("data\empire.jpg").convert("L"))
+im1 = array(Image.open("data/sf_view1.jpg").convert("L"))
+im2 = array(Image.open("data/sf_view2.jpg").convert("L"))
 
-harrisim = harris.compute_harris_response(im)
-filtered_coords = harris.get_harris_points(harrisim, 6)
-harris.plot_harris_points(im, filtered_coords)
+wid = 5
+harrisim = harris.compute_harris_response(im1, 5)
+filtered_coords1 = harris.get_harris_points(harrisim, wid+1)
+d1 = harris.get_descriptors(im1, filtered_coords1, wid)
+
+harrisim = harris.compute_harris_response(im2, 5)
+filtered_coords2 = harris.get_harris_points(harrisim, wid+1)
+d2 = harris.get_descriptors(im2, filtered_coords2, wid)
+
+print("starting matching")
+matches = harris.match_twosided(d1, d2)
+
+figure()
+gray()
+harris.plot_matches(im1, im2, filtered_coords1, filtered_coords2, matches)
+show()
